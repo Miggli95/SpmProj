44f386d0cb95b48531de77d2562ccc478fd2fd40
Victor <victorhaeggman@gmail.com>
2017-03-08 22:44:05 +0100

    3d enemy, goes after waypoints, chases player if gets close enough. No combat code yet.

---
+++ Assets/Scripts/3d/3d enemies/EnemyState.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+
+public interface EnemyState
+{
+    void Enter();
+
+    EnemyStateData Update(Vector3 pos, float deltaTime, NavMeshAgent agent);
+
+    void Exit();
+}
