326a745bee5a2183c01d9e637dce7ade82714e73
Victor <victorhaeggman@gmail.com>
2017-03-08 20:34:11 +0100

    Expanded on 2D AI code adding idle and patrol stage while cleaning up deal and death stage. Patrol is done with empty game objects positioned on the X value you want the enemy to chase after and then placed into an array. All this can be done through the unity UI without entering the code.

--- Assets/Scripts/2d/EnemyAI2D.cs
+++ Assets/Scripts/2d/EnemyAI2D.cs
@@ -7,35 +7,100 @@ public class EnemyAI2D : MonoBehaviour {
     public GameObject Player;
 
     public float MoveSpeed = 3.0f;
+    public float PatrolRangeX = 10;
+    public float PatrolRangeY = 10;
     public float AggroRangeX=5;
     public float AggroRangeY = 2;
     public int damage;
+
+    public Transform[] points;
+    private int destPoint = 0;
+
+
     private bool isDead = false;
+    private string currentState;
     private Rigidbody enemy;
+    private float timetoDeath;
     void Start () {
         enemy = GetComponent<Rigidbody>();
+        currentState = "idle";
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (isDead== false)
-        {
-            bool gotcommand = false;
-            if (Mathf.Abs(Player.transform.position.x - transform.position.x) <= AggroRangeX && Mathf.Abs(Player.transform.position.y - transform.position.y) <= AggroRangeY)
-            {
-                if (Player.transform.position.x > transform.position.x)
-                    enemy.velocity = new Vector3(MoveSpeed, enemy.velocity.y, enemy.velocity.z);
-                else
-                    enemy.velocity = new Vector3(-MoveSpeed, enemy.velocity.y, enemy.velocity.z);
-                gotcommand = true;
-            }
-            if (gotcommand == false)
-                enemy.velocity = new Vector3(0, 0, 0);
+        calcState();
+        switch (currentState) {
+            case "idle":
+                idle();
+                break;
+            case "patrol":
+                patrol();
+                break;
+            case "deal":
+                deal();
+                break; 
+            case "dead":
+                dead();
+                break;
         }
     }
+
+    private void calcState()
+    {
+        if (Mathf.Abs(Player.transform.position.x - transform.position.x) <= PatrolRangeX && Mathf.Abs(Player.transform.position.y - transform.position.y) <= PatrolRangeY)
+            if (Mathf.Abs(Player.transform.position.x - transform.position.x) <= AggroRangeX && Mathf.Abs(Player.transform.position.y - transform.position.y) <= AggroRangeY)
+            {
+                setSpeed(2);
+                currentState = "deal";
+            }
+            else
+            {
+                setSpeed(1);
+                currentState = "patrol";
+            }
+        else
+            currentState = "idle";
+
+    }
+
+    private void idle()
+    {
+        enemy.velocity = new Vector3(0, 0, 0);
+    }
+    private void patrol()
+    {
+        if (points.Length == 0)
+            return;
+        if (points[destPoint].position.x > transform.position.x)
+            enemy.velocity = new Vector3(MoveSpeed, enemy.velocity.y, enemy.velocity.z);
+        else
+            enemy.velocity = new Vector3(-MoveSpeed, enemy.velocity.y, enemy.velocity.z);
+        if(Mathf.Abs(points[destPoint].position.x - transform.position.x) <= 0.1)
+            destPoint = (destPoint + 1) % points.Length;
+    }
+    private void deal()
+    {
+        if (Player.transform.position.x > transform.position.x)
+            enemy.velocity = new Vector3(MoveSpeed, enemy.velocity.y, enemy.velocity.z);
+        else
+            enemy.velocity = new Vector3(-MoveSpeed, enemy.velocity.y, enemy.velocity.z);
+    }
+    private void dead()
+    {
+        timetoDeath -= Time.deltaTime;
+        if (timetoDeath <= 0)
+            Destroy(gameObject);
+    }
+
+
+
     public int getDamage()
     {
         return damage;
+    }
+    public void setSpeed(int newSpeed)
+    {
+        MoveSpeed = newSpeed;
     }
     public void deathAni()
     {
@@ -43,8 +108,10 @@ public class EnemyAI2D : MonoBehaviour {
         
         BoxCollider boxy = GetComponent<BoxCollider>();
         boxy.enabled= false;
-        isDead = true;
+
+        timetoDeath = 3;
+        currentState = "dead";
         enemy.velocity = new Vector3(0, -2, 0);
         Debug.Log("Killed enemy");
-    }
+    }        
 }
