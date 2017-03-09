b00dfa4ade9785756ffa9125dfdb7435694669e4
sauronman1 <aboud-malki@hotmail.com>
2017-03-09 14:15:21 +0100

    hublevel
    Har gjort en hublevel. Hittils har den en kub som använder designs 2d-controller och dörrarna kan inte användas en. Jag har också satt en Audio med två ljudfiler som vi kan nog använda oss av

---
+++ SpmProj-master/Assets/Scripts/3d/3d enemies/Deal.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System;
+
+public class Deal : EnemyState
+{
+    private EnemyAI3D ai;
+    private NavMeshAgent agent;
+
+    public Deal(EnemyAI3D ai)
+    {
+        if (ai == null)
+        {
+            throw new ArgumentNullException("no AI");
+        }
+        this.ai = ai;
+    }
+    public void Enter()
+    {
+        //do things that you do when you start chasing
+    }
+    public EnemyStateData Update(Vector3 pos, float deltaTime, NavMeshAgent agent)
+    {
+        agent.destination = pos;
+        var enemyStateData = GetEnemyStateData(pos);
+        return enemyStateData;
+        
+    }
+    private EnemyStateData GetEnemyStateData(Vector3 pos)
+    {
+        var enemyStateData = new EnemyStateData();
+        enemyStateData.Pos = pos;
+        if (agent.remainingDistance > 20f)
+        {
+            enemyStateData.NewState = new Patrol(ai);
+        }
+        return enemyStateData;
+    }
+
+    public void Exit()
+    {
+        //do things you do when you stop chasing
+    }
+
+
+}
---
+++ SpmProj-master/Assets/Scripts/3d/3d enemies/EnemyAI3D.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class EnemyAI3D : MonoBehaviour
+{
+    public GameObject Player;
+
+    public float MoveSpeed = 3.0f;
+    public float PatrolRangeX = 10;
+    public float PatrolRangeY = 10;
+    public float PatrolRangeZ = 10;
+    public float AggroRangeX = 5;
+    public float AggroRangeY = 2;
+    public float AggroRangeZ = 5;
+    public int damage;
+
+    private NavMeshAgent agent;
+    public Transform[] points;
+    public int destPoint = 0;
+
+
+    private bool isDead = false;
+    private EnemyState enemyState;
+    private Rigidbody enemy;
+    private float timetoDeath;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        enemy = GetComponent<Rigidbody>();
+        enemyState = GetInitialEnemyState();
+        enemyState.Enter();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var enemyStateData = enemyState.Update(Player.transform.position, Time.deltaTime, agent);
+        //calcState(enemyStateData);
+        if (enemyStateData.NewState != null)
+        {
+            ChangeEnemyState(Player.transform.position, enemyStateData);
+        }
+    }
+
+    private void ChangeEnemyState(Vector3 pos, EnemyStateData enemyStateData)
+    {
+        enemyState.Exit();
+        enemyState = enemyStateData.NewState;
+        enemyState.Enter();
+        if (enemyStateData.RunNewStateSameUpdate)
+        {
+            enemyState.Update(pos, Time.deltaTime, agent);
+        }
+    }
+
+   /* private void calcState(EnemyStateData enemyStateData)
+    {
+        if (Mathf.Abs(Player.transform.position.x - transform.position.x) <= PatrolRangeX && Mathf.Abs(Player.transform.position.y - transform.position.y) <= PatrolRangeY && Mathf.Abs(Player.transform.position.z - transform.position.z) <= PatrolRangeZ)
+            if (Mathf.Abs(Player.transform.position.x - transform.position.x) <= AggroRangeX && Mathf.Abs(Player.transform.position.y - transform.position.y) <= AggroRangeY && Mathf.Abs(Player.transform.position.y - transform.position.y) <= AggroRangeZ)
+            {
+                enemyStateData.NewState = new Deal(this);
+            }
+            else
+            {
+                enemyState = new Patrol(this);
+            }
+        else
+            currentState = "idle";
+
+    }
+
+    private void idle()
+    {
+        enemy.velocity = new Vector3(0, 0, 0);
+    }
+    private void patrol()
+    {
+        if (points.Length == 0)
+            return;
+        if (points[destPoint].position.x > transform.position.x)
+            enemy.velocity = new Vector3(MoveSpeed, enemy.velocity.y, enemy.velocity.z);
+        else
+            enemy.velocity = new Vector3(-MoveSpeed, enemy.velocity.y, enemy.velocity.z);
+        if (Mathf.Abs(points[destPoint].position.x - transform.position.x) <= 0.1)
+            destPoint = (destPoint + 1) % points.Length;
+    }
+    private void deal()
+    {
+        if (Player.transform.position.x > transform.position.x)
+            enemy.velocity = new Vector3(MoveSpeed, enemy.velocity.y, enemy.velocity.z);
+        else
+            enemy.velocity = new Vector3(-MoveSpeed, enemy.velocity.y, enemy.velocity.z);
+    }
+    private void dead()
+    {
+        timetoDeath -= Time.deltaTime;
+        if (timetoDeath <= 0)
+            Destroy(gameObject);
+    }
+
+
+
+    public int getDamage()
+    {
+        return damage;
+    }
+    public void setSpeed(int newSpeed)
+    {
+        MoveSpeed = newSpeed;
+    }
+    public void deathAni()
+    {
+        enemy.constraints = RigidbodyConstraints.FreezePositionX;
+
+        BoxCollider boxy = GetComponent<BoxCollider>();
+        boxy.enabled = false;
+
+        timetoDeath = 3;
+        currentState = "dead";
+        enemy.velocity = new Vector3(0, -2, 0);
+        Debug.Log("Killed enemy");
+    } */
+    private EnemyState GetInitialEnemyState()
+    {
+        EnemyState enemyState = null;
+        if (Mathf.Abs(Player.transform.position.x - transform.position.x) <= PatrolRangeX && Mathf.Abs(Player.transform.position.y - transform.position.y) <= PatrolRangeY && Mathf.Abs(Player.transform.position.z - transform.position.z) <= PatrolRangeZ)
+            if (Mathf.Abs(Player.transform.position.x - transform.position.x) <= AggroRangeX && Mathf.Abs(Player.transform.position.y - transform.position.y) <= AggroRangeY && Mathf.Abs(Player.transform.position.y - transform.position.y) <= AggroRangeZ)
+            {
+                enemyState = new Deal(this);
+            }
+            else
+            {
+                enemyState = new Patrol(this);
+            }
+        else
+            enemyState= new  Patrol(this);
+        return enemyState;
+    }
+    public void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+
+
+
+}
