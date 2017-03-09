d88423c8101402d4a5ffa48cc6fc29a838752f6f
sauronman1 <aboud-malki@hotmail.com>
2017-03-09 15:05:12 +0100

    testar git

---
+++ SpmProj-master/Assets/Scripts/3d/3d enemies/Patrol.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System;
+
+public class Patrol : EnemyState
+{
+    private EnemyAI3D ai;
+    private NavMeshAgent agent;
+
+    public Patrol(EnemyAI3D ai)
+    {
+        if (ai == null)
+        {
+            throw new ArgumentNullException("no AI");
+        }
+        this.ai = ai;
+        ai.GotoNextPoint();
+    }
+    public void Enter()
+    {
+        //do things that you do when you start patrolling
+    }
+    public EnemyStateData Update(Vector3 pos, float deltaTime, NavMeshAgent agent)
+    {
+        if (agent.remainingDistance < 0.5f)
+            ai.GotoNextPoint();
+        bool inRange = CalcDistance(pos);
+        var enemyStateData = GetEnemyStateData(pos, inRange);
+        return enemyStateData;
+    }
+    private bool CalcDistance(Vector3 pos)
+    {
+        return (Mathf.Abs(ai.gameObject.transform.position.x - pos.x) <= ai.AggroRangeX && Mathf.Abs(ai.gameObject.transform.position.y - pos.y) <= ai.AggroRangeY && Mathf.Abs(ai.gameObject.transform.position.z - pos.z) <= ai.AggroRangeZ);     
+    }
+    private EnemyStateData GetEnemyStateData(Vector3 pos, bool inRange)
+    {
+        var enemyStateData = new EnemyStateData();
+        enemyStateData.Pos = pos;
+        if (inRange)
+        {
+            enemyStateData.NewState = new Deal(ai);
+        }
+        return enemyStateData;
+    }
+    public void Exit()
+    {
+        //do things you do when you stop patrolling
+    }
+
+
+
+    
+}
