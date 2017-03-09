34c399155cde42a2889cb9ee06c09a2282a79bf7
sync93 <timpaboi@live.se>
2017-03-09 14:39:17 +0100

    Revert "Revert "hublevel""
    This reverts commit 2c8b741b0f0c4addc3a29be0103dcbd60595bf53.

---
+++ SpmProj-master/Assets/Scripts/2d/player2d_controller.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2d_controller : MonoBehaviour
+{
+    public float speed = 50f;
+    public float jumpPower = 150f;
+
+    public int jumpCount;
+    public bool grounded;
+    private Rigidbody _rigi;
+
+    public Animator anim;
+    public AudioClip run_sound;
+    public AudioClip jump_sound;
+    public AudioClip hurt_sound;
+
+    public Transform blood;
+    // Use this for initialization
+    void Start()
+    {
+        _rigi = transform.GetComponent<Rigidbody>();
+        anim = gameObject.GetComponent<Animator>();
+        blood.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //moving the player
+        _rigi.AddForce((Vector2.right * speed) * h);
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount <= 2)
+        {
+            _rigi.AddForce(Vector3.up * (jumpPower * _rigi.mass * 2f));
+            jumpCount++;
+            anim.SetBool("Grounded", false);
+        }
+        // GetComponent<MeshRenderer>().flipX = h.x < 0 ? true : false;
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+
+        switch (col.gameObject.tag)
+        {
+            case "ground":
+                Debug.Log("123");
+                anim.SetBool("Grounded", true);
+                jumpCount = 0;
+                break;
+            case "spike":
+                Debug.Log("Dead");
+                blood.GetComponent<ParticleSystem>().enableEmission = true;
+                StartCoroutine(stopBlood());
+                break;
+            case "enemy":
+                if(col.gameObject.transform.position.y- transform.position.y <= -0.2f)
+                 {
+                    col.gameObject.GetComponent<EnemyAI2D>().deathAni();
+                    _rigi.AddForce(Vector3.up * (jumpPower * _rigi.mass * 2f));
+                }
+                else
+                    Debug.Log(col.gameObject.GetComponent<EnemyAI2D>().getDamage());
+                break;
+        }
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        switch (col.gameObject.tag)
+        {
+
+            case "spike":
+                Debug.Log("Trigger Dead");
+                break;
+        }
+
+    }
+
+    IEnumerator stopBlood()
+    {
+        yield return new WaitForSeconds(1f);
+        blood.GetComponent<ParticleSystem>().enableEmission = false;
+
+    }
+
+}
