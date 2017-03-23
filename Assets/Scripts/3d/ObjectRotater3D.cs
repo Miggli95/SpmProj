a1920a5a98a466a3e07bbc75febf18f731a3c9fe
xxhakuyaxx <xxhakuyaxx@gmail.com>
2017-03-23 15:32:16 +0100

    3D Level Rotation
    Now with invisible walls, get it now 9,99 USD only o3o

---
+++ Assets/Scripts/3d/ObjectRotater3D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RotationDirection{
+	clockwise,
+	counterClockwise
+}
+
+
+
+public class ObjectRotater3D : MonoBehaviour {
+	
+	[SerializeField] private RotationDirection direction;
+	[SerializeField] private float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rotationSpeed == 0)
+			return;
+		else
+			RotateObject ();
+		
+	}
+
+	private void RotateObject(){
+		if (direction == RotationDirection.clockwise)
+			transform.Rotate (Vector3.up * (Time.deltaTime * rotationSpeed), Space.World);
+		else if (direction == RotationDirection.counterClockwise)
+			transform.Rotate (Vector3.up * (Time.deltaTime * rotationSpeed) * (-1), Space.World);
+		else {
+			Debug.LogError ("<color=red>Error:</color> Rotation direction error");
+			return;
+		}
+	}
+
+
+}
