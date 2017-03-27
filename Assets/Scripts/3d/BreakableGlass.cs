3504d89d73083349510c761e7bb36ad1d5fc8918
xxhakuyaxx <xxhakuyaxx@gmail.com>
2017-03-27 23:01:42 +0200

    Breakable Glass
    Now it breaks

--- Assets/Scripts/3d/BreakableGlass.cs
+++ Assets/Scripts/3d/BreakableGlass.cs
@@ -5,28 +5,45 @@ using UnityEngine;
 public class BreakableGlass : MonoBehaviour
 {
 
-    public GameObject Player;
-    private MeshCollider Meshcol;
+	[SerializeField] private GameObject player;
+	[SerializeField] private float fallSpeed;
+	[SerializeField] private float breakTimer;
 
-    private void Start()
-    {
-        Meshcol = GetComponent<MeshCollider>();
+    private MeshCollider meshCol;
+	private bool smashed;
+
+    private void Start() {
+        meshCol = GetComponent<MeshCollider>();
+
+		if (fallSpeed == 0||breakTimer == 0)
+			throw new UnityException ("<color=red>Error: Breakable glass falling speed or break timer cannot be equal to zero.</color>");
+
+		smashed = false;
     }
 
     
-   
+	void FixedUpdate(){
+		if (smashed)
+			SmashGlass ();
+	}
 
 
     
     void OnTriggerEnter(Collider other)
     {
-        if (other == Player.GetComponent<SphereCollider>())
-        {
-            Debug.Log("triggad");
-
-            Meshcol.enabled = false;
-
+        if (other == player.GetComponent<SphereCollider>()) {
+			Debug.Log("<color=blue>Mechanic:</color> Breakable glass.");
+            meshCol.enabled = false;
+			smashed = true;
         }
     }
+
+	private void SmashGlass(){
+		breakTimer -= Time.deltaTime;
+		if (breakTimer > 0)
+			transform.Translate (Vector3.down * Time.deltaTime * fallSpeed, Space.World);
+		else
+			Destroy (gameObject);
+	}
 
 }
