eb9d8465b1a7c5702c15bcd05cdb6ae17b7a27ff
xxhakuyaxx <xxhakuyaxx@gmail.com>
2017-03-27 23:43:00 +0200

    3D Level Exit
    Boss level incoming!

---
+++ Assets/Scripts/3d/LevelExit3D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExit3D : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other == player.GetComponent<SphereCollider>()) {
+			Debug.Log("<color=blue>Mechanic:</color> Load Boss Level.");
+			Application.LoadLevel("BossLevel");
+		}
+	}
+
+}
