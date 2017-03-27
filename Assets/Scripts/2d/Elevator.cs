cc9179f04c0bbfe5b49d9abf7a2832848ff20d3f
sync93 <timpaboi@live.se>
2017-03-27 15:35:26 +0200

    Revert "revertEle"
    This reverts commit 1d33ffc71ca4c15355310e0762fa44caf816a3a8.

---
+++ Assets/Scripts/2d/Elevator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("player"))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.collider.CompareTag("player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
