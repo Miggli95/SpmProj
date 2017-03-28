e2c0b0399269c935154983db8f279e6734c3315a
Kan <atthasit.khan@gmail.com>
2017-03-29 00:47:42 +0200

    Trigger sign pref and collosion sign

---
+++ Assets/Scripts/2d/DisplayUI.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DisplayUI : MonoBehaviour {
+    public string myString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+   
+    // Use this for initialization
+    void Start () {
+        myText = GameObject.Find("CollisionText").GetComponent<Text>();
+        myText.color = Color.clear;
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        FadeText();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.CompareTag("player"))
+        {
+            displayInfo = true;
+        }
+    }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.collider.CompareTag("player"))
+        {
+            displayInfo = false;
+        }
+    }
+
+    void FadeText() {
+        if (displayInfo)
+        {
+            myText.text = myString;
+            myText.GetComponent<Text>().enabled = true;
+           // myText.color = Color.Lerp(myText.color, Color.red, fadeTime * Time.deltaTime);
+        }
+        else if (!displayInfo) {
+            myText.GetComponent<Text>().enabled = false;
+           // myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+
+    }
+}
--- Assets/Scripts/2d/TextSign.cs
+++ Assets/Scripts/2d/TextSign.cs
@@ -3,23 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TextSign : MonoBehaviour {
-    public GameObject CollText1, Triggertext2;
+    public GameObject CollText1;
 
 	// Use this for initialization
 	void Start () {
         CollText1.SetActive(false);
-        Triggertext2.SetActive(false);
+       
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       
+
+
+
+    }
     IEnumerator OnTriggerEnter(Collider other) {
         if (other.CompareTag("player")) {
-            Triggertext2.SetActive(true);
+            TextOn();
             yield return new WaitForSeconds(3f);
-            Triggertext2.SetActive(false);
+            TextOff();
         }
         
     }
