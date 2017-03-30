1b6e91310a101c350dfdfccdd42afb8b1ea26bae
sync93 <timpaboi@live.se>
2017-03-30 23:09:23 +0200

    added partcle to chckpoints, adjsuted prefab

--- Assets/Scripts/2d/MakeNewCheckPoint.cs
+++ Assets/Scripts/2d/MakeNewCheckPoint.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MakeNewCheckPoint : MonoBehaviour {
-
+    public GameObject checkP2;
+    public GameObject checkP3;
+    public GameObject checkParticle;
     public player2d_controller playerScript;
 
     // Use this for initialization
@@ -18,10 +20,15 @@ public class MakeNewCheckPoint : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
 
+        Destroy(checkParticle);
+        Destroy(checkP2);
+        Destroy(checkP3);
         playerScript.currentCheckPoint= gameObject;
+
+
        // if (other.name == "player")
         //{
-         //   playerScript.currentCheckPoint = gameObject;
+         //   playerScript.currentCheckPoint = gameObject; 
         //}
     }
 }
