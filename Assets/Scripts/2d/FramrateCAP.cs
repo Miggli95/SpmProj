f47a7546a19e5be200b4c6a0308f0052178d854e
Kan <atthasit.khan@gmail.com>
2017-04-10 14:51:31 +0200

    FramrateCap script

---
+++ Assets/Scripts/2d/FramrateCAP.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FramrateCAP : MonoBehaviour {
+    public int vSync;
+    public int frameRate;
+    // Use this for initialization
+    void Awake () {
+        QualitySettings.vSyncCount = vSync;
+        Application.targetFrameRate = frameRate;
+        }
+	
+	
+}
