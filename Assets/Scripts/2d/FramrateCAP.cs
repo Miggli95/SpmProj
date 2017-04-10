e2bfff0e79a8ff98695c6988154a3ac08f2ab5d2
Kan <atthasit.khan@gmail.com>
2017-04-10 23:01:25 +0200

    add movefps

--- Assets/Scripts/2d/FramrateCAP.cs
+++ Assets/Scripts/2d/FramrateCAP.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class FramrateCAP : MonoBehaviour {
     public int vSync;
     public int frameRate;
+
+    Resolution res;
     // Use this for initialization
     void Awake () {
-        QualitySettings.vSyncCount = vSync;
-        Application.targetFrameRate = frameRate;
+        res = Screen.currentResolution;
+        if (res.refreshRate == 60) {
+            QualitySettings.vSyncCount = 1;
+           
+        }
+        if (res.refreshRate == 120)
+        {
+            QualitySettings.vSyncCount = 2;
+        }
+
+        Debug.Log(QualitySettings.vSyncCount);
+        //QualitySettings.vSyncCount = vSync;
+        //Application.targetFrameRate = frameRate;
         }
 	
 	
