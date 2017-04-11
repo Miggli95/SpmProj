cb5029d802a79e88f726eaa4195c5c6b1fa75796
Kan <atthasit.khan@gmail.com>
2017-04-11 15:13:56 +0200

    footstep sound test

--- Assets/Scripts/2d/StaticFootstep.cs
+++ Assets/Scripts/2d/StaticFootstep.cs
@@ -3,18 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StaticFootstep : MonoBehaviour {
-   private AudioSource source;
-   public AudioClip clip;
-	// Use this for initialization
-	void Start () {
-        source = GetComponent<AudioSource>();
-	}
+   private AudioSource[] sources;
+    private AudioSource sources1;
+    private AudioSource sources2;
+    public AudioClip clip;
+    public AudioClip clip2;
+    // Use this for initialization
+    void Start () {
+        sources = GetComponents<AudioSource>();
+        sources1 = sources[0];
+        sources2 = sources[1];
+        AnimationEvent ae = new AnimationEvent();
+        ae.messageOptions = SendMessageOptions.DontRequireReceiver;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-    void PlayStaticFootstepSound() {
-        source.PlayOneShot(clip);
+        if (sources2.isPlaying)
+            sources2.Stop();
+    }
+    void PlayStaticFootstepSound()
+    {
+        if (sources1.isPlaying)
+        {
+            sources2.Stop();
+        }
+        if (!sources2.isPlaying)
+        {
+            sources2.PlayOneShot(clip);
+        }
+    }
+    void PlayStaticFootstepSound2()
+    {
+        if (sources1.isPlaying)
+        {
+            sources2.Stop();
+        }
+
+        if (!sources2.isPlaying)
+        {
+            sources2.PlayOneShot(clip2);
+        }
+    }
+    void Stop() {
+        sources2.Stop();
+
     }
 }
