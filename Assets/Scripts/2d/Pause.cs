a3f3855de1211db134aa9bfa02eb582788e183eb
Kan <atthasit.khan@gmail.com>
2017-04-16 20:26:41 +0200

    Esc for Pause

---
+++ Assets/Scripts/2d/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    private bool paused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
