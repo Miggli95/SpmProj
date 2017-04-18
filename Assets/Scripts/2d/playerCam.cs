bdd3dee2868c264931d00d831609db5df220b393
Miggli95 <miggli@hotmail.se>
2017-04-18 18:54:33 +0200

    smoother camera movement
    i inspektorn för kamera skriptet kan man nu ställa in offset för
    kameran. och även hur stor deadzone ska vara och hur långt tid det ska
    ta för kameran att följa efter spelaren när den är utanför deadzone

--- Assets/Scripts/2d/playerCam.cs
+++ Assets/Scripts/2d/playerCam.cs
@@ -6,20 +6,40 @@ public class playerCam : MonoBehaviour {
 
     public GameObject player;      
 
-    private Vector3 offset;         
-
-    
+    public Vector3 offset;
+    Vector3 to;
+    public Vector2 deadZone;
+    private Vector3 velocity;
+    public float time;
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        if(offset == null)
         offset = transform.position - player.transform.position;
     }
 
     
     void LateUpdate()
     {
+       velocity = Vector3.zero;//player.GetComponent<Rigidbody>().velocity;
+       Vector3 targetPos = player.transform.position;
+      
+        if (Mathf.Abs(transform.position.x - targetPos.x) > deadZone.x)
+        {
+            to.x = targetPos.x;
+        }
+
+
+        if (Mathf.Abs(transform.position.y - targetPos.y) > deadZone.y)
+        {
+            to.y = targetPos.y;
+        }
+
+
+
+        transform.position = Vector3.SmoothDamp(transform.position, to + offset,ref velocity, time);
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        //transform.position = player.transform.position + offset;
     }
 }
 
