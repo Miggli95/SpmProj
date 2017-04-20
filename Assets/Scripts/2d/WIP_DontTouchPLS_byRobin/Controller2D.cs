3cdcb349e3493c1fd1af78946cc389cba67f01cd
xxhakuyaxx <xxhakuyaxx@gmail.com>
2017-04-21 01:34:24 +0200

    Level2 backup and Raycast 2D controller
    Raycast 2d controller now is able to detect collisions and do
    single/double jump.*
    * one known bug...
    Level 2 backup with some design, not tested.

---
+++ Assets/Scripts/2d/WIP_DontTouchPLS_byRobin/Controller2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CharacterController2D{
+	[RequireComponent (typeof (BoxCollider2D))]
+	public partial class Controller2D : MonoBehaviour {
+
+		BoxCollider2D playerCollider;
+
+		void Start () {
+			playerCollider = GetComponent<BoxCollider2D> ();
+			CalculateRaySpacing ();
+		}
+	
+		void Update () {
+			
+		}
+
+
+	}
+
+}
