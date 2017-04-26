5378e7c149f0b8bf55a039e1f2c1ac4f0aba7d4f
Kan <atthasit.khan@gmail.com>
2017-04-26 18:43:28 +0200

    Particle package*still bugged with old unity version *

---
+++ Assets/NonMesh/Particle group/Particles/Slam/CheckpointParticle/Rotate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+    public int z;
+    public int x;
+    public int y;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+    }
+}
