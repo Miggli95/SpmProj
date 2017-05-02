8eb3a10f4afc805d9f5cd49c2b44d18fa7ebc947
sync93 <timpaboi@live.se>
2017-05-02 23:12:32 +0100

    major upd what me and Kan did in school lvl3

---
+++ Assets/reminder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reminder : MonoBehaviour {
+    public GameObject obj1;
+    public GameObject obj2;
+
+
+    
+	// Use this for initialization
+	void Start () {
+        obj1.SetActive(false);
+        obj2.SetActive(false);
+    }
+	
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    IEnumerator OnTriggerStay()
+    {
+        yield return new WaitForSeconds(5f);
+        obj1.SetActive(true);
+        obj2.SetActive(true);
+    }
+
+    
+
+}
