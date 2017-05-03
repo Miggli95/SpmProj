a9ec499411396cfaef588695431a2c6ca85d8ba2
Victor <victorhaeggman@gmail.com>
2017-05-03 19:21:43 +0200

    Added my own boss music Sorry Kan, also replanaced the audio on enemy 1

---
+++ Assets/MyUnitySingleton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyUnitySingleton : MonoBehaviour {
+    private static MyUnitySingleton instance = null;
+    public static MyUnitySingleton Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name != "BossLevel" || SceneManager.GetActiveScene().name != "BossLevel 2" || SceneManager.GetActiveScene().name != "BossLevel 3")
+        {
+            Destroy(this);
+        }
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+   void Update()
+    {
+    }
+}
