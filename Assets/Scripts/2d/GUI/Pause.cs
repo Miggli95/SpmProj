e227f98e0db484a551197566adb1f2281655a272
笹川　白矢 <xxhakuyaxx@gmail.com>
2017-05-03 15:07:36 +0200

    Resolved Pause cs 13 Null Reference
    Resolved Null Reference when pause menu being called.
    -------------------------------------------------------------------
    Known Issue:
    Level 3 does not register mouse click, thus click on pause does not
    bring up the pause menu.

--- Assets/Scripts/2d/GUI/Pause.cs
+++ Assets/Scripts/2d/GUI/Pause.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Pause : MonoBehaviour
 {
-
-    private bool paused = false;
+ /*
+	private bool paused = false;
     GameObject canvas;
 
     private void Start()
@@ -43,5 +44,79 @@ public class Pause : MonoBehaviour
             Flip();
             ph.OnUnPause();
         }
-    }
+    }*/
+
+	[SerializeField] private GameObject pauseMenu;
+	private bool paused;
+
+	void Start(){
+		if (pauseMenu == null) {
+			findPauseMenu (ref pauseMenu);
+		}
+
+		paused = false;
+		pauseMenu.SetActive(false);
+	}
+
+	void Awake(){
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			paused = !paused;
+			if (paused)
+				ShowPauseMenu ();
+			else
+				HidePauseMenu ();
+		}
+	}
+
+
+
+	public void ShowPauseMenu(){
+		paused = true;
+		Time.timeScale = 0;
+		pauseMenu.SetActive (true);
+		Debug.Log ("<color=green>Pause Menu: </color>Paused.");
+	}
+
+	public void HidePauseMenu(){
+		paused = false;
+		Time.timeScale = 1;
+		pauseMenu.SetActive (false);
+		Debug.Log ("<color=green>Pause Menu: </color>Unpaused.");
+	}
+
+	public void LoadHub(){
+		paused = false;
+		Time.timeScale = 1;
+		pauseMenu.SetActive (false);
+		Application.LoadLevel(3);
+	}
+
+
+
+
+	private void findPauseMenu(ref GameObject varMenu){
+		if (GameObject.Find ("PausePanel")) {
+			varMenu = GameObject.Find ("PausePanel");
+			Debug.Log ("<color=green>Menu: </color>Pause menu found.");
+			return;
+		}
+
+		//search in inactive game objects in scene
+		foreach(GameObject objectInScene in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[]){
+			if (objectInScene.name == "PausePanel") {
+				varMenu = objectInScene;
+				Debug.Log ("<color=green>Menu: </color>Pause menu found as an inactive gameobject.");
+				return;
+			}
+		}
+
+		throw new ArgumentNullException ("<color=red>Pause Menu Error: </color>No pause menu found.");
+	}
+
+
+
+
 }
---
+++ Assets/Scripts/UnusedScripts_BackUp/PauseHud.cs
@@ -0,0 +1,36 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class PauseHud : MonoBehaviour {
+//    public GameObject pauseButton,hubButton, pausePanel;
+//
+//	private TitleEventManger titleCard;
+//
+//
+//	// Use this for initialization
+//	void Start () {
+//        pausePanel.SetActive(false);
+//    }
+//	
+//	// Update is called once per frame
+//	
+//    public void OnPause() {
+//        pausePanel.SetActive(true);
+//      
+//        Time.timeScale = 0;
+//
+//    }
+//    public void OnUnPause()
+//    {
+//        pausePanel.SetActive(false);
+//        
+//        Time.timeScale = 1;
+//    }
+//
+//    public void LoadHub() {
+//        Time.timeScale = 1;
+//
+//        Application.LoadLevel(3);
+//    }
+//}
