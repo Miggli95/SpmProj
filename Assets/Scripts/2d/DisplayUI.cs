33e3ac034e4935da6e26867b6d227cc06247dbee
笹川　白矢 <xxhakuyaxx@gmail.com>
2017-05-03 19:30:56 +0200

    Resolved DisplayUI cs NullReferenceException
    Resolved Null reference, loads a game object instead of text
    - changed level 1 and level 2 exit text display (you need a key!).
    please test.

--- Assets/Scripts/2d/DisplayUI.cs
+++ Assets/Scripts/2d/DisplayUI.cs
@@ -1,58 +1,97 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿///*******************************
+// * NullReferenceException: Object reference not set to an instance of an object
+// * DisplayUI.Start () (at Assets/Scripts/2d/DisplayUI.cs:15)
+// *
+// * -sesolved- 
+// * *******************************/
+//
+//
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//
+//
+//public class DisplayUI : MonoBehaviour {
+//    public string textToDisplay;
+//    public Text targetTextDisplayObject;
+//    public float fadeTime;
+//    public bool displayInfo;
+//   
+//    // Use this for initialization
+//    void Start () {
+//        targetTextDisplayObject = GameObject.Find("CollisionText").GetComponent<Text>();
+//        targetTextDisplayObject.color = Color.clear;	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+//
+//    void FixedUpdate()
+//    {
+//        FadeText();
+//    }
+//
+//    void OnCollisionEnter(Collision col)
+//    {
+//        if (col.collider.CompareTag("player"))
+//        {
+//            displayInfo = true;
+//        }
+//    }
+//    void OnCollisionExit(Collision col)
+//    {
+//        if (col.collider.CompareTag("player"))
+//        {
+//            displayInfo = false;
+//        }
+//    }
+//
+//    void FadeText() {
+//        if (displayInfo)
+//        {
+//            targetTextDisplayObject.text = textToDisplay;
+//            targetTextDisplayObject.GetComponent<Text>().enabled = true;
+//           // myText.color = Color.Lerp(myText.color, Color.red, fadeTime * Time.deltaTime);
+//        }
+//        else if (!displayInfo) {
+//            targetTextDisplayObject.GetComponent<Text>().enabled = false;
+//           // myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+//        }
+//
+//    }
+//}
+//
+
+
 using UnityEngine;
-using UnityEngine.UI;
 
-
+[RequireComponent( typeof(BoxCollider), typeof(BoxCollider))]
 public class DisplayUI : MonoBehaviour {
-    public string myString;
-    public Text myText;
-    public float fadeTime;
-    public bool displayInfo;
-   
-    // Use this for initialization
-    void Start () {
-        myText = GameObject.Find("CollisionText").GetComponent<Text>();
-        myText.color = Color.clear;
-        	
+	[SerializeField] private GameObject textDisplayObject;
+
+
+
+	void Start(){
+		if (textDisplayObject == null) {
+			throw new UnityException ("<color=red>Exit Text Error: </color>Text is null.");
+		}
+
+		textDisplayObject.SetActive (false);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void OnTriggerEnter(Collider col){
+		if (col.CompareTag ("player")) {
+			textDisplayObject.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if (col.CompareTag ("player")) {
+			textDisplayObject.SetActive (false);
+		}
+	}
 		
-	}
-
-    void FixedUpdate()
-    {
-        FadeText();
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.collider.CompareTag("player"))
-        {
-            displayInfo = true;
-        }
-    }
-    void OnCollisionExit(Collision col)
-    {
-        if (col.collider.CompareTag("player"))
-        {
-            displayInfo = false;
-        }
-    }
-
-    void FadeText() {
-        if (displayInfo)
-        {
-            myText.text = myString;
-            myText.GetComponent<Text>().enabled = true;
-           // myText.color = Color.Lerp(myText.color, Color.red, fadeTime * Time.deltaTime);
-        }
-        else if (!displayInfo) {
-            myText.GetComponent<Text>().enabled = false;
-           // myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
-        }
-
-    }
 }
