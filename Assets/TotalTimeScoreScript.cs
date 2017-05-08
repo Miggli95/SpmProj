db0afaaa5f4d19ac92b94b74e16b80ff2c217e53
Miggli95 <miggli@hotmail.se>
2017-05-08 19:24:46 +0200

    total time skrivs nu ut på highscore listan

---
+++ Assets/TotalTimeScoreScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TotalTimeScoreScript : MonoBehaviour
+{ 
+
+
+    public int levelNumber;
+    public float bestTotalTime;        // The player's score.
+    public float totalTime;
+    public float currentTime;
+    public GameManager manager;
+    public Text totalTimeText;                      // Reference to the Text component.
+    public Text bestTotalTimeText;
+    public Text currentTimeText;
+    void Start()
+    {
+        bestTotalTime = manager.getBestTotalTime();
+        totalTime = manager.getTotalTime();
+        currentTime = manager.getCurrentTime();
+
+    }
+
+    void Update()
+    {
+        // Set the displayed text 
+        bestTotalTimeText.text = "" + bestTotalTime;
+        totalTimeText.text = "" + totalTime;
+        currentTimeText.text = "" + currentTime;
+    }
+}
+
+
+
