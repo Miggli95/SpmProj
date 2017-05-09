76af9af6bfa66c30e0857abdeb6d419992ee384c
Miggli95 <miggli@hotmail.se>
2017-05-09 13:09:54 +0200

    Fixade BackToHub Knappen i end credits

---
+++ Assets/BackToHub.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToHub : MonoBehaviour
+{
+    public void LoadHub()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
