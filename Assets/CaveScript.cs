bae5077d96a4a0de9284ac000f96416987cee920
Kan <atthasit.khan@gmail.com>
2017-05-16 23:31:12 +0200

    Cave adjest

---
+++ Assets/CaveScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveScript : MonoBehaviour {
+
+    public GameObject mainCamera;
+    public GameObject caveCamera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+ void OnTriggerEnter(Collider other)
+    {
+        mainCamera.SetActive(false);
+        caveCamera.SetActive(true);
+    }
+ void OnTriggerExit(Collider other)
+    {
+        caveCamera.SetActive(false);
+        mainCamera.SetActive(true);
+    }
+}
